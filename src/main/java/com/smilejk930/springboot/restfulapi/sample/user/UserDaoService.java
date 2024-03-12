206e18254caaf0f07a6dd2c04e34fe89de40c514
smilejk <smilejk930@gmail.com>
2024-03-13 03:11:18 +0900

    Feat: 삭제 기능 구현

--- src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserDaoService.java
+++ src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.smilejk930.springboot.restfulapi.sample.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -29,8 +30,14 @@ public class UserDaoService {
     }
 
     public User findOne(int id) {
+        // Predicate는 조건을 나타내는 함수형 인터페이스로, 주어진 조건에 따라 참 또는 거짓을 반환합니다.
         // Predicate<? super User> predicate = user -> user.getId() == id;
         // 해당 ID를 가진 사용자를 찾아 반환하거나, 해당 ID에 해당하는 사용자가 없을 경우 null을 반환합니다.
         return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId() == id;
+        users.removeIf(predicate);
     }
 }
--- src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserResource.java
+++ src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,11 @@ public class UserResource {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
         return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
     }
 
     @PostMapping("/users")
