801a4308b6f279ac3a895f7290120fa9dbd9ef75
smilejk <smilejk930@gmail.com>
2024-03-13 02:15:28 +0900

    Doc: 코드 주석 추가

--- src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserDaoService.java
+++ src/main/java/com/smilejk930/springboot/restfulapi/sample/user/UserDaoService.java
@@ -30,6 +30,7 @@ public class UserDaoService {
 
     public User findOne(int id) {
         // Predicate<? super User> predicate = user -> user.getId() == id;
+        // 해당 ID를 가진 사용자를 찾아 반환하거나, 해당 ID에 해당하는 사용자가 없을 경우 null을 반환합니다.
         return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
     }
 }
