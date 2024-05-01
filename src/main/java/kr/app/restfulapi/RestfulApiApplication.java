2ace382c2b9ffab9002ec300800cac820176a78d
kjk <jinkwang930@ictway.co.kr>
2024-05-01 15:49:07 +0900

    Refactor: eclipse-java-google-style
    formatter 적용

--- src/main/java/kr/app/restfulapi/RestfulApiApplication.java
+++ src/main/java/kr/app/restfulapi/RestfulApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RestfulApiApplication.class, args);
+  }
 
 }
