bb0ac09b953c0083f2086a83f34accde93ef3e6e
kjk <jinkwang930@ictway.co.kr>
2024-05-01 15:31:40 +0900

    first commit

---
+++ src/main/java/kr/app/restfulapi/RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package kr.app.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
