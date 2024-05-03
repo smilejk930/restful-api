8250d84b687fef6954569c80d6951dab9ca7bb5a
kjk <jinkwang930@ictway.co.kr>
2024-05-03 11:05:26 +0900

    Refactor: User DTO Valid 조건 추가
    @Size, @Past

--- src/main/java/kr/app/restfulapi/sample/user/User.java
+++ src/main/java/kr/app/restfulapi/sample/user/User.java
@@ -1,6 +1,8 @@
 package kr.app.restfulapi.sample.user;
 
 import java.time.LocalDate;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +13,13 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class User {
+
   private Integer id;
+
+  @Size(min = 2, message = "이름은 2글자 이상 입력해주세요.")
   private String name;
+
+  // past는 현재 날짜보다 과거인지를 검증하는 어노테이션입니다.
+  @Past(message = "생년월일은 과거 날짜만 입력 가능합니다.")
   private LocalDate birthDate;
 }
