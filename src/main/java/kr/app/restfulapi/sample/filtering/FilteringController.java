2cdcc72a2b0265abfc044cdd38cca5481b70788a
kjk <jinkwang930@ictway.co.kr>
2024-05-10 14:34:32 +0900

    Feat: response의 정적필터를 동적필터로 변경
    메소드에따라 필터링되는 필드를 다르게 구성할 수 있음

--- src/main/java/kr/app/restfulapi/sample/filtering/FilteringController.java
+++ src/main/java/kr/app/restfulapi/sample/filtering/FilteringController.java
@@ -2,20 +2,40 @@ package kr.app.restfulapi.sample.filtering;
 
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
   @GetMapping("/filtering")
-  public SomeBean filtering() {
-    return new SomeBean("value1", "value2", "value3");
+  public MappingJacksonValue filtering() {
+    SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+    MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+    SimpleBeanPropertyFilter fileter =
+        SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");// field1, field3만 보여줘라
+    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", fileter);
+    mappingJacksonValue.setFilters(filters);
+
+    return mappingJacksonValue;
   }
 
   @GetMapping("/filtering-list")
-  public List<SomeBean> filteringList() {
-    return Arrays.asList(new SomeBean("value1", "value2", "value3"),
+  public MappingJacksonValue filteringList() {
+    List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
         new SomeBean("value3", "value4", "value5"));
+
+    MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+    SimpleBeanPropertyFilter fileter =
+        SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");// field2, field3만 보여줘라
+    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", fileter);
+    mappingJacksonValue.setFilters(filters);
+
+    return mappingJacksonValue;
   }
 }
--- src/main/java/kr/app/restfulapi/sample/filtering/SomeBean.java
+++ src/main/java/kr/app/restfulapi/sample/filtering/SomeBean.java
@@ -1,18 +1,18 @@
 package kr.app.restfulapi.sample.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@JsonFilter("SomeBeanFilter")
 // @JsonIgnoreProperties({"field1", "field2"})
 public class SomeBean {
-  @JsonIgnore // response의 필드를 정적으로 표시되지 않게 설정
+  // @JsonIgnore // response의 필드를 정적으로 표시되지 않게 설정
   private String field1;
 
-  @JsonProperty("field_2") // response 필드명을 custom할 수 있음
+  // @JsonProperty("field_2") // response 필드명을 custom할 수 있음
   private String field2;
   private String field3;
 }
