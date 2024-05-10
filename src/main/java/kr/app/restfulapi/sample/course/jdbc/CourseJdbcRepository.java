9997dc8c56e4c3216b773efa951477fcdc7a2363
kjk <jinkwang930@ictway.co.kr>
2024-05-10 16:21:35 +0900

    Feat: course 테이블에 insert 구문 생성

---
+++ src/main/java/kr/app/restfulapi/sample/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,21 @@
+package kr.app.restfulapi.sample.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+  @Autowired
+  private JdbcTemplate springjdbcTemplate;
+
+  private static String INSERT_QUERY = """
+          insert into course (id, name, author)
+          values(1, 'Learn AWS', 'smilejk930');
+      """;
+
+  public void insert() {
+    springjdbcTemplate.update(INSERT_QUERY);
+  }
+}
