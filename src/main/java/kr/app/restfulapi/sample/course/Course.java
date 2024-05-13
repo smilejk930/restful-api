fae4cc7a8772fce8ec6a073237fa5ec39c78bf3c
smilejk930 <smilejk930@gmail.com>
2024-05-13 15:29:19 +0900

    Feat: @NoArgsConstructor 기본 생성자 lombok 추가

--- src/main/java/kr/app/restfulapi/sample/course/Course.java
+++ src/main/java/kr/app/restfulapi/sample/course/Course.java
@@ -4,17 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @Entity
 public class Course {
-
-  public Course() {}
 
   @Id
   private long id;
