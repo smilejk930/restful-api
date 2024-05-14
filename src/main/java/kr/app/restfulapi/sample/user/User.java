0faad987eb510b56ba0ec06d163a5fc51546bb67
smilejk930 <smilejk930@gmail.com>
2024-05-14 17:40:28 +0900

    Feat: User 엔터티와 일대다 관계로 Post 엔터티 생성
    @OneToMany, @ManyToOne 사용해서 일대다 관계 작성

--- src/main/java/kr/app/restfulapi/sample/user/User.java
+++ src/main/java/kr/app/restfulapi/sample/user/User.java
@@ -1,12 +1,15 @@
 package kr.app.restfulapi.sample.user;
 
 import java.time.LocalDate;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import kr.app.restfulapi.sample.user.post.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,7 +17,7 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
+// @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Entity(name = "user_details")
@@ -32,4 +35,18 @@ public class User {
   @Past(message = "생년월일은 과거 날짜만 입력 가능합니다.")
   // @JsonProperty("birth_date")
   private LocalDate birthDate;
+
+  @OneToMany(mappedBy = "user") // 속성으로 특정 관계를 가지는 필드의 매핑설정
+  @JsonIgnore // 응답을 외부로 표현하지 않게 하기 위해 설정
+  private List<Post> posts; // 사용자는 게시물과 일대다 관계
+
+  public User(Integer id, @Size(min = 2, message = "이름은 2글자 이상 입력해주세요.") String name,
+      @Past(message = "생년월일은 과거 날짜만 입력 가능합니다.") LocalDate birthDate) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.birthDate = birthDate;
+  }
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(10002, current_date(), '김진광');
 
 insert into user_details(id, birth_date, name)
 values(10003, current_date(), '홍길동');
+
+insert into post(id, description, user_id)
+values(20001, 'I want to lean AWS', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I want to lean DevOps', 10001);
+
+insert into post(id, description, user_id)
+values(20003, 'I want to lean Restful API', 10002);
+
+insert into post(id, description, user_id)
+values(20004, 'I want to lean Next.js', 10003);
