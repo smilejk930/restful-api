fe264e73dff94e55a5736096a0ced23dbc8e7622
smilejk930 <smilejk930@gmail.com>
2024-06-04 16:45:58 +0900

    Feat: CORS 요청 활성화 하기
    WebMvcConfigurer에서 Cross Origin Requests 활성화

--- src/main/java/kr/app/restfulapi/RestfulApiApplication.java
+++ src/main/java/kr/app/restfulapi/RestfulApiApplication.java
@@ -2,6 +2,9 @@ package kr.app.restfulapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulApiApplication {
@@ -9,5 +12,19 @@ public class RestfulApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestfulApiApplication.class, args);
   }
+
+  // Cross Origin Requests
+  // Allow all requests only form http://localhost:3000/
+  @Bean
+  public WebMvcConfigurer corsConfigure() {
+    return new WebMvcConfigurer() {
+      @Override
+      // 특정한 패턴에 대해 Cross Origin Requests 처리
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
+
 
 }
