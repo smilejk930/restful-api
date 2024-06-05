6224e97700f657d016bc803b2be69bdc24c5382d
smilejk930 <smilejk930@gmail.com>
2024-06-05 10:24:25 +0900

    Refactor: SpringSecurity가 작동하지 않도록 설정
    JWT Security 사용을 위해 스캔되지 않게 설정

--- src/main/java/kr/app/restfulapi/security/SpringSecurityConfiguration.java
+++ src/main/java/kr/app/restfulapi/security/SpringSecurityConfiguration.java
@@ -2,13 +2,12 @@ package kr.app.restfulapi.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class SpringSecurityConfiguration {
 
   @Bean
