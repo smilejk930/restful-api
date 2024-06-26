8ed909156fdf7b0c9ddf6dada6786a7b1d709a71
smilejk930 <smilejk930@gmail.com>
2024-06-26 09:24:12 +0900

    Feat: entity에서 @GeneratedValue 전략 유의
    지연로딩(FetchType.LAZY)에서 GenerationType.IDENTITY을 사용하면 에러가 발생함. 따라서
    GenerationType.AUTO 사용

---
+++ src/main/java/kr/app/restfulapi/uga/post/entity/Post.java
@@ -0,0 +1,39 @@
+package kr.app.restfulapi.uga.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.app.restfulapi.uga.user.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+
+  @Id
+  // Resolved [java.lang.NullPointerException: Cannot invoke "cubrid.jdbc.driver.CUBRIDConnection.createCUBRIDException(int, java.lang.Throwable)"
+  // because "this.con" is null]
+  // @GeneratedValue(strategy = GenerationType.IDENTITY) //cubrid error 발생
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  private String title;
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+}
