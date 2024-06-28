52c261c9a2e731a3223f9bd3e201ec6fe9380d1f
smilejk930 <smilejk930@gmail.com>
2024-06-28 12:37:42 +0900

    Feat: valid 조건 추가
    @NotBlank

--- src/main/java/kr/app/restfulapi/uga/post/dto/PostDto.java
+++ src/main/java/kr/app/restfulapi/uga/post/dto/PostDto.java
@@ -1,13 +1,14 @@
 package kr.app.restfulapi.uga.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import kr.app.restfulapi.uga.post.entity.Post;
 import kr.app.restfulapi.uga.user.entity.User;
 
 public record PostDto(
     String postId,
-    @Size(min = 3, max = 200, message = "제목은 {min}자 이상, {max}자 이하로 입력해주세요.") String sj,
-    @Size(min = 1, max = 4000, message = "내용은 {min}자 이상, {max}자 이하로 입력해주세요.") String cn) {
+    @NotBlank @Size(min = 3, max = 200, message = "제목은 {min}자 이상, {max}자 이하로 입력해주세요.") String sj,
+    @NotBlank @Size(min = 1, max = 4000, message = "내용은 {min}자 이상, {max}자 이하로 입력해주세요.") String cn) {
 
   static public PostDto toDto(Post post) {
     return new PostDto(post.getPostId(), post.getSj(), post.getCn());
