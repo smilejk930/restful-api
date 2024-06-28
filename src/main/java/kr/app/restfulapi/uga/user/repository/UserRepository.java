56192780307ec2796ce66959195ec63f1522970a
smilejk930 <smilejk930@gmail.com>
2024-06-28 12:20:04 +0900

    Feat: userId를 String 타입으로 변경

--- src/main/java/kr/app/restfulapi/uga/user/repository/UserRepository.java
+++ src/main/java/kr/app/restfulapi/uga/user/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import kr.app.restfulapi.uga.user.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
   // Optional<User> findByUsername(String username);
 }
