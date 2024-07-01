684d8387b6199504435b949d167da133a99131da
smilejk930 <smilejk930@gmail.com>
2024-07-01 12:16:03 +0900

    Feat: post 비지니스 로직의 성공과 실패에 대한 처리

--- src/main/java/kr/app/restfulapi/uga/post/controller/PostController.java
+++ src/main/java/kr/app/restfulapi/uga/post/controller/PostController.java
@@ -2,12 +2,15 @@ package kr.app.restfulapi.uga.post.controller;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,6 +25,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
+import kr.app.restfulapi.response.error.exception.ResourceNotFoundException;
+import kr.app.restfulapi.response.success.SuccessResponse;
+import kr.app.restfulapi.response.success.SuccessStatus;
 import kr.app.restfulapi.uga.post.dto.PostDto;
 import kr.app.restfulapi.uga.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -34,49 +40,59 @@ public class PostController {
   private final PostService postService;
 
   @GetMapping
-  public ResponseEntity<Page<PostDto>> getAllPost(@ModelAttribute PostDto postDto,
+  public ResponseEntity<SuccessResponse> getAllPost(@ModelAttribute PostDto postDto,
       @PageableDefault(size = 10, sort = "registDt", direction = Sort.Direction.DESC) Pageable pageable, @AuthenticationPrincipal UserDetails userDetails) {
 
-    Page<PostDto> posts = postService.getAllPosts(postDto, pageable, userDetails);
+    Page<PostDto> postDtoList = postService.getAllPosts(postDto, pageable, userDetails);
 
-    return ResponseEntity.ok(posts);
+    List<EntityModel<PostDto>> models = postDtoList.stream().map(data -> {
+      Link detailLink = null;
+      detailLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPostById(data.postId(), userDetails)).withRel("detailLink");
+      return EntityModel.of(data, detailLink);
+    }).toList();
+
+    return ResponseEntity.ok(SuccessResponse.builder().status(SuccessStatus.OK).data(models).build());
   }
 
   @GetMapping("/{postId}")
-  public ResponseEntity<EntityModel<PostDto>> getPostById(@PathVariable String postId, @AuthenticationPrincipal UserDetails userDetails) {
+  public ResponseEntity<SuccessResponse> getPostById(@PathVariable String postId, @AuthenticationPrincipal UserDetails userDetails) {
 
     Optional<PostDto> optPostDto = postService.getPostById(postId, userDetails);
 
-    return optPostDto.map(postDto -> {
-      EntityModel<PostDto> model = EntityModel.of(postDto);
-      model.add(linkTo(methodOn(this.getClass()).getPostById(postId, userDetails)).withSelfRel());
-      model.add(linkTo(methodOn(this.getClass()).getAllPost(postDto, Pageable.unpaged(), userDetails)).withRel("allPosts"));
+    return optPostDto.map(data -> {
+      EntityModel<PostDto> model = EntityModel.of(data);
+      model.add(linkTo(methodOn(this.getClass()).getAllPost(data, Pageable.unpaged(), userDetails)).withRel("allPosts"));
 
-      return ResponseEntity.ok(model);
-    }).orElse(ResponseEntity.notFound().build());
+      return ResponseEntity.ok(SuccessResponse.builder().status(SuccessStatus.OK).data(model).build());
+    }).orElseThrow(ResourceNotFoundException::new);
   }
 
   @PostMapping
-  public ResponseEntity<PostDto> createPost(@RequestBody @Valid PostDto postDto, @AuthenticationPrincipal UserDetails userDetails) {
+  public ResponseEntity<SuccessResponse> createPost(@RequestBody @Valid PostDto postDto, @AuthenticationPrincipal UserDetails userDetails) {
 
-    PostDto createdPost = postService.createPost(postDto, userDetails);
+    PostDto createdPostDto = postService.createPost(postDto, userDetails);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.builder().status(SuccessStatus.CREATED).data(createdPostDto).build());
   }
 
   @PutMapping("/{postId}")
-  public ResponseEntity<PostDto> updatePost(@PathVariable String postId, @RequestBody @Valid PostDto postDto, @AuthenticationPrincipal UserDetails userDetails) {
+  public ResponseEntity<SuccessResponse> updatePost(@PathVariable String postId, @RequestBody @Valid PostDto postDto, @AuthenticationPrincipal UserDetails userDetails) {
 
-    Optional<PostDto> updatedPost = postService.updatePost(postId, postDto, userDetails);
+    Optional<PostDto> updatedPostDto = postService.updatePost(postId, postDto, userDetails);
 
-    return updatedPost.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    return updatedPostDto.map(data -> {
+      return ResponseEntity.ok(SuccessResponse.builder().status(SuccessStatus.UPDATED).data(data).build());
+    }).orElseThrow(ResourceNotFoundException::new);
   }
 
   @DeleteMapping("/{postId}")
-  public ResponseEntity<Void> deletePost(@PathVariable String postId, @AuthenticationPrincipal UserDetails userDetails) {
+  public ResponseEntity<SuccessResponse> deletePost(@PathVariable String postId, @AuthenticationPrincipal UserDetails userDetails) {
 
-    boolean deleted = postService.deletePost(postId, userDetails);
+    boolean isDeleted = postService.deletePost(postId, userDetails);
 
-    return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    return Optional.ofNullable(isDeleted)
+        .filter(Boolean::booleanValue)
+        .map(data -> ResponseEntity.ok(SuccessResponse.builder().status(SuccessStatus.DELETED).build()))
+        .orElseThrow(ResourceNotFoundException::new);
   }
 }
