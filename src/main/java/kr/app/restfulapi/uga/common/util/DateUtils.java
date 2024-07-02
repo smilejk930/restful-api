03ce8c652cb30eaac1f127415ff030567682596f
smilejk930 <smilejk930@gmail.com>
2024-07-02 18:12:04 +0900

    Feat: Date Util 생성 및 테스트 케이스 추가

---
+++ src/main/java/kr/app/restfulapi/uga/common/util/DateUtils.java
@@ -0,0 +1,41 @@
+package kr.app.restfulapi.uga.common.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DateUtils {
+
+  public static String getCurrentDate() {
+    LocalDate now = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    return now.format(formatter);
+  }
+
+  public static String getCurrentYear() {
+    LocalDate now = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+    return now.format(formatter);
+  }
+
+  public static String getCurrentMonth() {
+    LocalDate now = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+    return now.format(formatter);
+  }
+
+  public static String getCurrentDay() {
+    LocalDate now = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
+    return now.format(formatter);
+  }
+
+  public static String getCurrentDateTimeMillisecond() {
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddhhmmssSSS");
+    return now.format(formatter);
+  }
+}
