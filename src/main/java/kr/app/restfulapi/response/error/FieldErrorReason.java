881e5be82d0e6836fd63702295f69c509a0c7bff
smilejk930 <smilejk930@gmail.com>
2024-07-02 18:13:33 +0900

    Feat: 파일확장자 허용 금지 reason 추가

--- src/main/java/kr/app/restfulapi/response/error/FieldErrorReason.java
+++ src/main/java/kr/app/restfulapi/response/error/FieldErrorReason.java
@@ -12,6 +12,9 @@ public enum FieldErrorReason {
   ),
   RESOURCE_NOT_FOUND(
       "정보가 존재하지 않습니다."
+  ),
+  FILE_EXTENSION_NOT_ALLOWED(
+      "허용되지 않는 파일 확장자입니다."
   );
 
   /** error가 발생한 항목의 이유 */
