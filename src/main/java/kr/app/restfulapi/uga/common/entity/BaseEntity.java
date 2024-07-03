d41ed14b75711bc8b373a0e1093c67b95cb15e8f
smilejk930 <smilejk930@gmail.com>
2024-07-03 18:21:05 +0900

    Feat: BaseEntity 분리
    필요에 따라 시간만 필요한 entity가 있을 수 있기에 분리시킨다.

--- src/main/java/kr/app/restfulapi/uga/common/entity/BaseEntity.java
+++ src/main/java/kr/app/restfulapi/uga/common/entity/BaseEntity.java
@@ -1,9 +1,7 @@
 package kr.app.restfulapi.uga.common.entity;
 
-import java.time.LocalDateTime;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
@@ -14,29 +12,18 @@ import lombok.Setter;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public abstract class BaseEntity extends BaseTimeEntity {
 
   @Comment("등록자아이디")
   @CreatedBy
   @Column(updatable = false, nullable = false)
   private String registerId;
 
-  @Comment("등록일시")
-  @CreatedDate
-  @Column(updatable = false)
-  private LocalDateTime registDt;
-
   @Setter
   @Comment("수정자아이디")
   // @LastModifiedBy
   @Column(insertable = false)
   private String updusrId;
-
-  @Setter
-  @Comment("수정일시")
-  // @LastModifiedDate
-  @Column(insertable = false)
-  private LocalDateTime updtDt;
 
   @Setter
   @Comment("등록서버명")
---
+++ src/main/java/kr/app/restfulapi/uga/common/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package kr.app.restfulapi.uga.common.entity;
+
+import java.time.LocalDateTime;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+  @Comment("등록일시")
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime registDt;
+
+  @Setter
+  @Comment("수정일시")
+  // @LastModifiedDate
+  @Column(insertable = false)
+  private LocalDateTime updtDt;
+}
