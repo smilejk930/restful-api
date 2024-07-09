12e908abcf295bcc4fc9bbf1e819bb521905a3a1
smilejk930 <smilejk930@gmail.com>
2024-07-09 11:53:42 +0900

    Feat: util 추가

---
+++ src/main/java/kr/app/restfulapi/global/util/CustomObjectUtils.java
@@ -0,0 +1,55 @@
+package kr.app.restfulapi.uga.common.util;
+
+import java.lang.reflect.Field;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import kr.app.restfulapi.response.error.exception.BusinessException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CustomObjectUtils {
+
+  /** 모든 속성값이 NULL인지 확인 */
+  public static boolean isAllNullOfFields(Object object) {
+    if (object == null) {
+      return true;
+    }
+
+    for (Field field : FieldUtils.getAllFields(object.getClass())) {
+      try {
+        Object fieldValue = FieldUtils.readField(field, object, true);
+        if (fieldValue != null) {
+          log.error(field.getName());
+          return false; // 하나라도 null이 아니면 false 반환
+        }
+      } catch (IllegalAccessException e) {
+        throw new BusinessException(e, "Failed to access field: " + field.getName());
+      }
+    }
+
+    return true; // 모든 필드가 null일 경우 true 반환
+  }
+
+  /** 속성값 중 NULL이 존재하는지 확인 */
+  public static boolean isAnyNullOfFields(Object object) {
+    if (object == null) {
+      return true;
+    }
+
+    for (Field field : FieldUtils.getAllFields(object.getClass())) {
+      try {
+        Object fieldValue = FieldUtils.readField(field, object, true);
+        if (fieldValue == null) {
+          log.error(field.getName());
+          return true; // 하나라도 null이 존재하면 true 반환
+        }
+      } catch (IllegalAccessException e) {
+        throw new BusinessException(e, "Failed to access field: " + field.getName());
+      }
+    }
+
+    return false; // 모든 필드가 null이 아닐 경우 false 반환
+  }
+}
