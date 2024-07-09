28eafc1f2ca06b0b2e6e02cf9c2e24a478de53cd
smilejk930 <smilejk930@gmail.com>
2024-07-09 11:55:56 +0900

    Feat: Qeurydsl 설정

---
+++ src/main/java/kr/app/restfulapi/global/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package kr.app.restfulapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory queryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
+}
