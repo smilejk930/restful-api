1a79e13a507c4edc8ba51cf06097972f3ed62046
smilejk930 <smilejk930@gmail.com>
2024-07-09 11:41:45 +0900

    Feat: custom한 properties 작성
    CurrencyServiceConfiguration 생성
    application-*.properties 생성
    
    getter/setter 사용을 위해 lombok 설치 필요함
    제대로 설치 안될때
    1. sts에서 lombok 라이브러리 찾고 오른쪽 버튼 > Run As > Java Application 실행
    2. sts 폴더 선택 후 Quit installer

---
+++ src/main/java/kr/app/restfulapi/domain/sample/edu/configuration/CurrencyConfigurationController.java
@@ -0,0 +1,20 @@
+package kr.app.restfulapi.domain.sample.edu.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * custom한 currency-service에 대해 properties를 확인하는 클래스
+ */
+@RestController
+public class CurrencyConfigurationController {
+
+  @Autowired
+  private CurrencyServiceConfiguration configuration;
+
+  @GetMapping(value = "/currency-service")
+  public CurrencyServiceConfiguration getCurrencyServiceConfiguration() {
+    return configuration;
+  }
+}
---
+++ src/main/java/kr/app/restfulapi/domain/sample/edu/configuration/CurrencyServiceConfiguration.java
@@ -0,0 +1,22 @@
+package kr.app.restfulapi.domain.sample.edu.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * custom한 currency-service에 대해 properties를 설정한다.
+ * currency-service.url
+ * currency-service.username
+ * currency-service.key
+ */
+@ConfigurationProperties(prefix = "currency-service")
+@Getter
+@Setter
+@Component
+public class CurrencyServiceConfiguration {
+  private String url;
+  private String username;
+  private String key;
+}
