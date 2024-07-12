a137e7cadc7027d017acba2d43e3ecfce42f07fc
smilejk930 <smilejk930@gmail.com>
2024-07-12 17:48:27 +0900

    Fix: 순환참조 오류 해결
    CustomUserDetailsService에서 UserRepository를 주입.
    PasswordEncoder를 직접 주입

--- src/main/java/kr/app/restfulapi/domain/common/user/service/CustomUserDetailsService.java
+++ src/main/java/kr/app/restfulapi/domain/common/user/service/CustomUserDetailsService.java
@@ -5,15 +5,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import kr.app.restfulapi.domain.common.user.dto.UserDto;
+import kr.app.restfulapi.domain.common.user.entity.User;
+import kr.app.restfulapi.domain.common.user.repository.UserRepository;
 import kr.app.restfulapi.domain.common.user.util.UserPrincipal;
+import kr.app.restfulapi.global.response.error.FieldErrorReason;
+import kr.app.restfulapi.global.response.error.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-  private final UserService userService;
+  private final UserRepository userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String userNm) throws UsernameNotFoundException {
@@ -21,8 +24,12 @@ public class CustomUserDetailsService implements UserDetailsService {
   }
 
   public UserDetails loadUserByLoginId(String loginId) {
-    Optional<UserDto> optUserDto = userService.getUserByLoginId(loginId);
+    Optional<User> optUser = userRepository.findByLoginId(loginId);
 
-    return UserPrincipal.create(optUserDto.get().toEntity());
+    if (optUser.isPresent()) {
+      return UserPrincipal.create(optUser.get());
+    } else {
+      throw new ResourceNotFoundException("loginId", loginId, FieldErrorReason.USER_NOT_FOUND);
+    }
   }
 }
--- src/main/java/kr/app/restfulapi/domain/common/user/service/UserService.java
+++ src/main/java/kr/app/restfulapi/domain/common/user/service/UserService.java
@@ -1,12 +1,12 @@
 package kr.app.restfulapi.domain.common.user.service;
 
 import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import kr.app.restfulapi.domain.common.user.dto.UserDto;
 import kr.app.restfulapi.domain.common.user.entity.User;
 import kr.app.restfulapi.domain.common.user.repository.UserRepository;
-import kr.app.restfulapi.global.config.SecurityConfig;
 import kr.app.restfulapi.global.response.error.FieldErrorReason;
 import kr.app.restfulapi.global.response.error.exception.InvalidPasswordException;
 import kr.app.restfulapi.global.response.error.exception.LoginIdAlreadyExistsException;
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
   private final UserRepository userRepository;
-  private final SecurityConfig securityConfig;
+  private final PasswordEncoder passwordEncoder;
 
   @Transactional(readOnly = true)
   public Optional<UserDto> getUserByLoginId(String loginId) {
@@ -41,7 +41,7 @@ public class UserService {
     User user = userDto.toEntity();
     // user.setPassword("{bcrypt}" + securityConfig.passwordEncoder().encode(user.getPassword()));
     // TODO 패스워드가 DB에 어떻게 저장되는지 확인 필요
-    user.setPassword(securityConfig.passwordEncoder().encode(user.getPassword()));
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
     User savedUser = userRepository.save(user);
 
     return UserDto.toDto(savedUser);
