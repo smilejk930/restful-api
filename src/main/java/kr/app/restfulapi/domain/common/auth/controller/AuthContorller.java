1441fa4838ced0fc8224e7d352964b36d2c1ad82
smilejk930 <smilejk930@gmail.com>
2024-07-12 16:36:02 +0900

    Feat: 사용자 로그인을 통한 토큰 발행과 사용자 등록 기능 구현

--- src/main/java/kr/app/restfulapi/domain/common/auth/controller/AuthContorller.java
+++ src/main/java/kr/app/restfulapi/domain/common/auth/controller/AuthContorller.java
@@ -1,13 +1,69 @@
 package kr.app.restfulapi.domain.common.auth.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import kr.app.restfulapi.domain.common.auth.dto.JwtResponseDto;
+import kr.app.restfulapi.domain.common.auth.dto.LoginDto;
+import kr.app.restfulapi.domain.common.user.dto.UserDto;
+import kr.app.restfulapi.domain.common.user.service.CustomUserDetailsService;
+import kr.app.restfulapi.domain.common.user.service.UserService;
+import kr.app.restfulapi.global.response.success.SuccessResponse;
+import kr.app.restfulapi.global.response.success.SuccessStatus;
+import kr.app.restfulapi.global.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthContorller {
 
+  private final AuthenticationManager authenticationManager;
+  private final JwtTokenProvider tokenProvider;
+  private final CustomUserDetailsService customUserDetailsService;;
+  private final UserService userService;
 
+  @PostMapping("/login")
+  public ResponseEntity<SuccessResponse> authenticateUser(@Valid @RequestBody LoginDto loginDto) throws Exception {
+    try {
+      UserDetails userDetails = customUserDetailsService.loadUserByLoginId(loginDto.loginId());
+
+      // TODO 정확한 이해가 필요함(userDetails.getUsername())
+      Authentication authentication =
+          authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDetails.getUsername(), loginDto.password()));
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+      String jwt = tokenProvider.generateToken(authentication);
+      log.info("User logged in successfully: {}", loginDto.loginId());
+      return ResponseEntity.ok(SuccessResponse.builder().status(SuccessStatus.OK).data(new JwtResponseDto(jwt)).build());
+    } catch (BadCredentialsException ex) {
+      log.warn("Login attempt failed for user: {}", loginDto.loginId());
+      throw new BadCredentialsException("Invalid loginId or password");
+    } catch (LockedException ex) {
+      log.warn("Locked account attempt to login: {}", loginDto.loginId());
+      throw new LockedException("Account is locked");
+    } catch (Exception ex) {
+      throw new Exception("An error occurred during login");
+    }
+  }
+
+  @PostMapping("/signup")
+  public ResponseEntity<SuccessResponse> createUser(@Valid @RequestBody UserDto userDto) {
+    UserDto createdUserDto = userService.createUser(userDto);
+    return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.builder().status(SuccessStatus.CREATED).data(createdUserDto).build());
+  }
 }
--- src/main/java/kr/app/restfulapi/domain/common/auth/dto/LoginDto.java
+++ src/main/java/kr/app/restfulapi/domain/common/auth/dto/LoginDto.java
@@ -1,11 +1,12 @@
 package kr.app.restfulapi.domain.common.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import kr.app.restfulapi.domain.common.user.entity.User;
 
 public record LoginDto(
     @NotBlank(message = "로그인아이디는 필수 입력값입니다.") String loginId,
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.") String password) {
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.") @Size(min = 6, max = 50, message = "비밀번호는 {min}자 이상, {max}자 이하로 입력해주세요.") String password) {
 
   public User toEntity() {
     return User.builder().loginId(loginId).password(password).build();
