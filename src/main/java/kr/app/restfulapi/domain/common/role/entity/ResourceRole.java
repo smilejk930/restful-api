f60c415176aac7c4dbd83db797528294e5ca40f5
smilejk <smilejk930@gmail.com>
2024-07-13 11:48:10 +0900

    Feat: getter , setter 추가

--- src/main/java/kr/app/restfulapi/domain/common/role/entity/ResourceRole.java
+++ src/main/java/kr/app/restfulapi/domain/common/role/entity/ResourceRole.java
@@ -8,9 +8,20 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import kr.app.restfulapi.domain.common.resource.entity.Resource;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "resource_roles", schema = "TESTUSER")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class ResourceRole {
 
   @Id
