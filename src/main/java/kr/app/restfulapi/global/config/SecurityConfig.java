15ffd6790047c235e2b02cad6c6c8748b73cb5ab
smilejk930 <smilejk930@gmail.com>
2024-07-17 14:33:31 +0900

    Feat: Security 설정
    filter 설정(jwt, resource, exception 처리 등)

--- src/main/java/kr/app/restfulapi/global/config/SecurityConfig.java
+++ src/main/java/kr/app/restfulapi/global/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package kr.app.restfulapi.global.config;
 
 import java.util.Arrays;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,14 +15,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import kr.app.restfulapi.domain.common.user.service.CustomUserDetailsService;
 import kr.app.restfulapi.global.filter.DynamicAuthorizationFilter;
+import kr.app.restfulapi.global.filter.GlobalExceptionTranslationFilter;
 import kr.app.restfulapi.global.filter.JwtAuthenticationFilter;
+import kr.app.restfulapi.global.response.error.exception.SecurityExceptionHandler;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -26,6 +32,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final SecurityExceptionHandler securityExceptionHandler;
+  private final GlobalExceptionTranslationFilter globalExceptionTranslationFilter;
   private final CustomUserDetailsService userDetailsService;
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
   private final DynamicAuthorizationFilter dynamicAuthorizationFilter;
@@ -35,12 +43,34 @@ public class SecurityConfig {
     http.csrf(csrf -> csrf.disable())
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(authz -> authz.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+        // .authorizeHttpRequests(authz -> authz.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+        // dynamicAuthorizationFilter에서 DB로 동적 조회를 통해 관리하기에 필요 없어짐.
+        .authenticationProvider(authenticationProvider())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         .addFilterAfter(dynamicAuthorizationFilter, JwtAuthenticationFilter.class)
+        .addFilterBefore(globalExceptionTranslationFilter, ExceptionTranslationFilter.class)// 필터에서의 전역 exception 처리
+        .exceptionHandling(exceptionHandling -> exceptionHandling
+            .authenticationEntryPoint(securityExceptionHandler) /* AuthenticationEntryPoint는 인증되지 않은 사용자가 보안된 리소스에 접근하려 할 때 호출 */
+            .accessDeniedHandler(securityExceptionHandler)) // AccessDeniedHandler는 인증된 사용자가 권한이 없는 리소스에 접근하려 할 때 호출
         .userDetailsService(userDetailsService); // UserDetailsService 설정
 
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public FilterRegistrationBean<GlobalExceptionTranslationFilter> globalExceptionTranslationFilterRegistration(
+      GlobalExceptionTranslationFilter filter) {
+    FilterRegistrationBean<GlobalExceptionTranslationFilter> registration = new FilterRegistrationBean<>(filter);
+    registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return registration;
   }
 
   @Bean
@@ -51,11 +81,6 @@ public class SecurityConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
-  }
-
-  @Bean
-  public AntPathMatcher antPathMatcher() {
-    return new AntPathMatcher();
   }
 
   /**
