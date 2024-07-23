3feb5c984532e89a48265044371cbcf5b6d882eb
smilejk930 <smilejk930@gmail.com>
2024-07-23 14:37:44 +0900

    Feat: tsid 생성 테스트 클래스 구현

---
+++ src/test/java/kr/app/restfulapi/global/util/TsidGenerationToStringTest.java
@@ -0,0 +1,16 @@
+package kr.app.restfulapi.global.util;
+
+import org.junit.jupiter.api.Test;
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class TsidGenerationToStringTest {
+
+  @Test
+  void test() {
+    for (int i = 0; i < 5; i++)
+      log.info(TSID.fast().toString());
+  }
+
+}
