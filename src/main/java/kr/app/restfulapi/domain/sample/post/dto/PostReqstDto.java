c5f47d613274b6d63351f03b82c5cf7409164608
smilejk <smilejk930@gmail.com>
2024-08-02 05:54:15 +0900

    Doc: valid 문구 변경

--- src/main/java/kr/app/restfulapi/domain/sample/post/dto/PostReqstDto.java
+++ src/main/java/kr/app/restfulapi/domain/sample/post/dto/PostReqstDto.java
@@ -12,7 +12,7 @@ public record PostReqstDto(
     @NotBlank(message = "내용은 필수 입력값입니다.", groups = {
         FinalSubmit.class})
     @Size(min = 1, max = 4000, message = "내용은 {min}자 이상, {max}자 이하로 입력해주세요.", groups = {FinalSubmit.class}) String cn,
-    @Min(value = 1, message = "전문길이는 최소 1이어야 합니다.") Integer telgmLen){
+    @Min(value = 1, message = "전문길이는 최소 {value}이어야 합니다.") Integer telgmLen){
 
   public Post toEntity() {
     return Post.builder().ttl(ttl).cn(cn).telgmLen(telgmLen).build();
