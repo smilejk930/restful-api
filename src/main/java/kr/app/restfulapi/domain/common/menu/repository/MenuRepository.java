0a6115d04a3972ca47f3a8a34d9994cbac6850ea
smilejk930 <smilejk930@gmail.com>
2024-08-02 15:10:30 +0900

    Feat: @EntityGraph 선언
    FetchType.LAZY로 설정된 menuAuthrts 조회 시 transaction 범위 밖에서
    사용(DynamicAuthorizationFilter)하여 LazyInitializationException 방지를 위해 선언

--- src/main/java/kr/app/restfulapi/domain/common/menu/repository/MenuRepository.java
+++ src/main/java/kr/app/restfulapi/domain/common/menu/repository/MenuRepository.java
@@ -3,6 +3,7 @@ package kr.app.restfulapi.domain.common.menu.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import kr.app.restfulapi.domain.common.menu.entity.Menu;
@@ -10,6 +11,8 @@ import kr.app.restfulapi.domain.common.menu.entity.Menu;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, String>, MenuRepositoryCustom {
 
+  @EntityGraph(attributePaths = {"menuAuthrts"}) // FetchType.LAZY로 설정된 menuAuthrts 조회 시 transaction 범위 밖에서 사용(DynamicAuthorizationFilter)하여
+                                                 // LazyInitializationException 방지를 위해 선언
   List<Menu> findAll();
 
   List<Menu> findAllByMenuGroupCd(String menuGroupCd, Sort sort);
