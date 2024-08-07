a3e8aafbcbdea617cdaa36055d91d64376d39772
smilejk930 <smilejk930@gmail.com>
2024-08-07 16:38:19 +0900

    Feat: 파일 엔터티가 추가될때마다 Type 추가
    업무 도메인과 관련되어 파일 데이터를 분류하는 그룹 명칭

---
+++ src/main/java/kr/app/restfulapi/domain/common/file/util/FileGroupNmType.java
@@ -0,0 +1,13 @@
+package kr.app.restfulapi.domain.common.file.util;
+
+/**
+ * <pre>
+ * 파일그룹명
+ * 업무 도메인과 관련되어 파일 데이터를 분류하는 그룹 명칭
+ * 파일 엔터티가 추가될때마다 Type 추가
+ * </pre>
+ */
+public enum FileGroupNmType {
+  /** 샘플게시판 */
+  SAMPLE_POST
+}
